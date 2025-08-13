c2249d641d7b53ce0bbaa16686083b8a98da14e6
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-08-13 16:17:14 +0100

    Add jb.sh header

---
+++ src/helpers/01_header.sh
@@ -0,0 +1,9 @@
+# In all scripts
+log "Monolithic jb.sh"
+log "Created by HackerDude"
+log "v$JB_VERSION"
+log
+log "  \"If you wish to make an apple pie from scratch"
+log "  you must first invent the universe\""
+log "- Carl Sagan"
+log
