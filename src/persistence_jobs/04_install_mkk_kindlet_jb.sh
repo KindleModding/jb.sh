420469ebd9359a84ad91385b08c7dd6ddaca50d6
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-08-13 16:17:54 +0100

    Missed one (oops)

--- src/persistence_jobs/04_install_mkk_kindlet_jb.sh
+++ src/persistence_jobs/04_install_mkk_kindlet_jb.sh
@@ -2,7 +2,7 @@
 
 # Kindlet JB doesn't match, install it
 if [ "$(md5sum "/opt/amazon/ebook/lib/json_simple-1.1.jar" | cut -d' ' -f1)" != "$(md5sum "/var/local/kmc/persistence/json_simple-1.1.jar" | cut -d' ' -f1)" ] ; then
-	logmsg "Copying the kindlet jailbreak"
+	log "Copying the kindlet jailbreak"
 	cp -f "/var/local/kmc/persistence/json_simple-1.1.jar" "/opt/amazon/ebook/lib/json_simple-1.1.jar"
 	chmod 0664 "/opt/amazon/ebook/lib/json_simple-1.1.jar"
 fi
