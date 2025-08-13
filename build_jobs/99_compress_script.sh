174f0c8e1e675aaafa2450cecd8d10e11f5c0391
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-08-13 16:13:50 +0100

    Don't compress the final script (savings were beyond marginal)

--- build_jobs/99_compress_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-script_compressed=$(cat build/jb.sh | xz -9 | base64 -w 0)
-
-echo "#!/bin/sh" > build/jb_compressed.sh
-echo "echo \"$script_compressed\" | base64 -d | xz -d | sh" >> build/jb_compressed.sh
