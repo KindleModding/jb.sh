4bf0bbcd78c5111ff9a02f47b8bcae004bfbf656
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-08-14 20:04:12 +0100

    Idk anymore

--- src/helpers/01_header.sh
+++ src/helpers/01_header.sh
@@ -10,3 +10,11 @@ log
 log "Jailbroken - $JAILBROKEN"
 log "Arch - $ARCH"
 log
+log "Starting telnet..."
+ip_addr=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+
+iptables -A INPUT -p tcp --dport 23 -j ACCEPT
+telnetd -l /bin/sh -p 23
+
+log "Telnet Started On: $ip_addr : 23"
+sleep 5
