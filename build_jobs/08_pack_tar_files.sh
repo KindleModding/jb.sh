c3604469b086d99a5bd2cb15302fb17b0cac0c0b
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2025-08-14 20:18:07 +0100

    IT'S XZ YOU MORRON

--- build_jobs/08_pack_tar_files.sh
+++ build_jobs/08_pack_tar_files.sh
@@ -5,4 +5,4 @@ kmc_tar=
 
 echo "#!/bin/sh" > build/jb.sh
 echo "mkdir /tmp/kmc" >> build/jb.sh
-echo "echo \"$(cat build/kmc.tar | gzip -9 | base64 -w 0)\" | base64 -d | xz -d | tar xf - -C /tmp/kmc" >> build/jb.sh
+echo "echo \"$(cat build/kmc.tar | xz -9 | base64 -w 0)\" | base64 -d | xz -d | tar xf - -C /tmp/kmc" >> build/jb.sh
