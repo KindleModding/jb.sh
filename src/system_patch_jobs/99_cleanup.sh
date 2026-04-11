4cd00afccc8333cb20c38f6fd565974d470f8b4e
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2026-04-11 13:09:41 +0100

    Add GUI restarting for manual runmode

---
+++ src/system_patch_jobs/99_cleanup.sh
@@ -0,0 +1,14 @@
+mntroot ro
+log "Done!"
+
+if [ $RUN_MODE -eq 0 ]; then
+log "Restarting gui..."
+sleep 2 # So they can read what's about to happen
+restart lab126_gui &
+sleep 3 # Wait for it to stop
+/var/local/kmc/bin/fbink -y 3 -m -S 7 "Restarting GUI"
+/var/local/kmc/bin/fbink -y 4 -m -S 7 "Please Wait..."
+/var/local/kmc/bin/fbink -y 16 -p -S 3 "(Kindles are slow lol)  "
+/var/local/kmc/bin/fbink -y -6 -m -S 4 "(Error dialog is fine)"
+/var/local/kmc/bin/fbink -y -5 -m -S 4 "(Just press close and keep waiting!)"
+fi
