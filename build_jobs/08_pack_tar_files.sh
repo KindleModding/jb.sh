778192b02b97221e6a6a5b042054f00ac65077ba
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2026-04-11 15:19:20 +0100

    Add cleanup handler

--- build_jobs/08_pack_tar_files.sh
+++ build_jobs/08_pack_tar_files.sh
@@ -4,5 +4,6 @@
 kmc_tar=
 
 echo "#!/bin/sh" > build/jb.sh
+echo "rm -rf /tmp/kmc" >> build/jb.sh
 echo "mkdir /tmp/kmc" >> build/jb.sh
 echo "echo \"$(cat build/kmc.tar | xz -9 | base64 -w 0)\" | base64 -d | xz -d | tar xf - -C /tmp/kmc" >> build/jb.sh
