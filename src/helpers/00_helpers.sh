21853c387318df03bd4655f7cf39720eeeab6b12
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2026-04-12 14:03:46 +0100

    Fix JB_HEADER not showing

--- src/helpers/00_helpers.sh
+++ src/helpers/00_helpers.sh
@@ -3,7 +3,7 @@
 ###
 # Defines
 ###
-JB_SH_VERSION="vE12"
+JB_SH_VERSION="vE13"
 
 #if [ ! -n "${JB_HEADER+x}" ] && [ -f "/var/local/jailbreak.txt" ]; then
 #    JB_HEADER=$(cat /var/local/jailbreak.txt)
