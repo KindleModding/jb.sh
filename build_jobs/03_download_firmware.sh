d58ac679f63ad2787246d418d87de87c67518995
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2026-06-21 16:41:12 +0100

    Update KPM

--- build_jobs/03_download_firmware.sh
+++ build_jobs/03_download_firmware.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 echo "Downloading firmware"
-if command -v aria2c >"$LOG" 2>&1
-then
-    aria2c -c -s 16 -x 16 -k 2M "https://www.amazon.com/update_Kindle_Oasis_10th_Gen" -o "cache/firmware.bin" > "$LOG" || curl --progress-bar -L -C - -o "cache/firmware.bin" "https://www.amazon.com/update_Kindle_Oasis_10th_Gen" > "$LOG"
-else
-    curl --progress-bar -L -C - -o "cache/firmware.bin" "https://www.amazon.com/update_Kindle_Oasis_10th_Gen" > "$LOG"
+if [ ! -f "cache/firmware.bin" ]; then
+    if command -v aria2c >"$LOG" 2>&1
+    then
+        aria2c -c -s 16 -x 16 -k 2M "https://www.amazon.com/update_Kindle_Oasis_10th_Gen" -o "cache/firmware.bin" > "$LOG" || curl --progress-bar -L -C - -o "cache/firmware.bin" "https://www.amazon.com/update_Kindle_Oasis_10th_Gen" > "$LOG"
+    else
+        curl --progress-bar -L -C - -o "cache/firmware.bin" "https://www.amazon.com/update_Kindle_Oasis_10th_Gen" > "$LOG"
+    fi
 fi
