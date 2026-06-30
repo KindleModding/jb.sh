570e883008d107acc2571606f0e0549f0801397a
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2026-06-30 20:26:48 +0100

    Fix cleanup issues

--- src/jb_sh_jobs/06_cleanup.sh
+++ src/jb_sh_jobs/06_cleanup.sh
@@ -14,25 +14,18 @@ if [ $RUN_MODE -eq 1 ] || [ $JAILBROKEN -eq 0 ]; then
     log "Restarting scanner..." # Necessary for sh_integration
     sleep 2 # So they can read what's about to happen
     if [ -f "/etc/upstart/kppmainapp.conf" ] ; then
-        restart scanner # For sh_integration
-        restart kppmainapp # For debug commands
-        restart pillow # Pillow handles the crash dialog so we can hide it by doing this
-        /usr/bin/xrefresh -d :0.0
+        restart lab126_gui &
+        sleep 3 # Wait for it to stop
     elif [ -f "/etc/upstart/acxe.conf" ] ; then
         restart acxe & # On older devices restart the entire gui
         sleep 3 # Wait for it to stop
-        /var/local/kmc/bin/fbink -y 3 -m -S 7 "Restarting GUI"
-        /var/local/kmc/bin/fbink -y 4 -m -S 7 "Please Wait..."
-        /var/local/kmc/bin/fbink -y 16 -p -S 3 "(Kindles are slow lol)  "
-        /var/local/kmc/bin/fbink -y -6 -m -S 4 "(Error dialog is fine)"
-        /var/local/kmc/bin/fbink -y -5 -m -S 4 "(Just press close and keep waiting!)"
     else
-        telinit 5
+        telinit 5 &
         sleep 3 # Wait for it to stop
-        /var/local/kmc/bin/fbink -y 3 -m -S 7 "Restarting GUI"
-        /var/local/kmc/bin/fbink -y 4 -m -S 7 "Please Wait..."
-        /var/local/kmc/bin/fbink -y 16 -p -S 3 "(Kindles are slow lol)  "
-        /var/local/kmc/bin/fbink -y -6 -m -S 4 "(Error dialog is fine)"
-        /var/local/kmc/bin/fbink -y -5 -m -S 4 "(Just press close and keep waiting!)"
     fi
+    /var/local/kmc/bin/fbink -y 3 -m -S 7 "Restarting GUI"
+    /var/local/kmc/bin/fbink -y 4 -m -S 7 "Please Wait..."
+    /var/local/kmc/bin/fbink -y 16 -p -S 3 "(Kindles are slow lol)  "
+    /var/local/kmc/bin/fbink -y -6 -m -S 4 "(Error dialog is fine)"
+    /var/local/kmc/bin/fbink -y -5 -m -S 4 "(Just press close and keep waiting!)"
 fi
